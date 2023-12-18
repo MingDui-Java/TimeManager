37c110725855f6f73ebc821ea31ba5e21f7e5881
Aintme187 <128763833+Aintme187@users.noreply.github.com>
2023-12-18 17:43:52 +0800

    Delete src directory

--- src/Yang/Test.java
+++
@@ -1,9 +0,0 @@
-//Calendar currentCalendarCopy = Calendar.getInstance();
-//        int daysInMonth = currentCalendarCopy.getActualMaximum(Calendar.DAY_OF_MONTH);
-//        int thisYear = currentCalendarCopy.get(Calendar.YEAR);
-//        int thisMonth = currentCalendarCopy.get(Calendar.MONTH);
-//        int thisDay = currentCalendar.get(Calendar.DAY_OF_MONTH);
-//        Map<Integer, DayPanel> dayMap = secondPanelMapByYear
-//        .get(currentCalendarCopy.get(Calendar.YEAR))
-//        .get(currentCalendarCopy.get(Calendar.MONTH));
-//        DayPanel panelForDay = dayMap.getOrDefault(i, null);
